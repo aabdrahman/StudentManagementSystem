4d9e3e3ebc9c0bec52d540f8e923159830baa77b
Abdrahman Akande <Abdrahman.Akande@parallexbank.com>
2024-12-08 20:51:06 +0100

    Committing a background service that runs every minute and logs the current time. This is in prearation for any possible future desired background service.

--- Program.cs
+++ Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService<StudentManagementBackgroundService>();
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
 builder.Services.ConfigureAuth();
---
+++ Services/StudentManagementBackgroundService.cs
@@ -0,0 +1,32 @@
+﻿
+using StudentManagementApplication.Interfaces;
+using StudentManagementApplication.Services;
+
+internal class StudentManagementBackgroundService : BackgroundService
+{
+    private readonly ILogger<StudentManagementBackgroundService> _logger;
+
+
+    public StudentManagementBackgroundService(ILogger<StudentManagementBackgroundService> logger)
+    {
+        _logger = logger;
+       // _studentManagementService = studentManagementService;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while(!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation($"Started at: {DateTime.Now}");
+            var _timer = new PeriodicTimer(TimeSpan.FromMinutes(1));
+            if (await _timer.WaitForNextTickAsync(stoppingToken))
+            {
+                _logger.LogInformation($"Running task at: {DateTime.Now}");
+                //var resp = await _studentManagementService.GetCourses();
+                _logger.LogInformation($"Run Success");
+            }
+
+            await Task.Delay(10000, stoppingToken);
+        }
+    }
+}
